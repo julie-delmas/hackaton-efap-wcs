9105efefafd9809fb0c65bcdb104ae3e7796d0df
Christophe Kuchmac <christophekuchmac@free.fr>
2018-12-21 00:58:48 +0100

    Working on view.

--- assets/js/quizz.js
+++ assets/js/quizz.js
@@ -1,40 +1,58 @@
 import '../scss/quizz.scss';
 
 // Array of all the questions and choices to populate the questions. This might be saved in some JSON file or a database and we would have to read the data in.
-var all_questions = [{
-    question_string: "What color is the sky?",
+let all_questions = [{
+    question_string: "Que représente la dépense énergétique de l'utilisation des réseaux sociaux en france ?",
     choices: {
-        correct: "Blue",
-        wrong: ["Pink", "Orange", "Green"]
+        correct: "40%",
+        wrong: ["20%", "60%", "80%"]
     }
 }, {
-    question_string: "Which season is the hottest?",
+    question_string: "Combien d'application sont \"actives\" dans notre smartphone dans qu'on ne les utilise réellement ?",
     choices: {
-        correct: "Summer",
-        wrong: ["Winter", "Autumn", "Spring"]
+        correct: "35",
+        wrong: ["17", "28", "5"]
     }
 }, {
-    question_string: "How many wheels are there on a tricycle?",
+    question_string: "Combien de Français ont accès à Internet en France ?",
     choices: {
-        correct: "Three",
-        wrong: ["One", "Two", "Four"]
+        correct: "88%",
+        wrong: ["56%", "67%", "100%"]
     }
 }, {
-    question_string: "test?",
+    question_string: "Combien de temps passent les Français par jour sur Internet ?",
     choices: {
-        correct: "Three",
-        wrong: ["One", "Two", "Four"]
+        correct: "4h48",
+        wrong: ["1h37", "6h32", "8h45"]
     }
 }, {
-    question_string: 'Who is the main character of Harry Potter?',
+    question_string: "Combien de temps passent les Français par jour sur les réseaux sociaux ?",
     choices: {
-        correct: "Harry Potter",
-        wrong: ["Hermione Granger", "Ron Weasley", "Voldemort"]
+        correct: "1h22",
+        wrong: ["2h43", "8h55", "12h54"]
+    }
+}, {
+    question_string: "Quel est le navigateur le plus utilisé dans le monde ?",
+    choices: {
+        correct: "Chrome",
+        wrong: ["Safari", "Firefox", "Bing"]
+    }
+}, {
+    question_string: "Quel est le site le plus visité dans le monde ?",
+    choices: {
+        correct: "Google",
+        wrong: ["You tube", "Facebook", "Baidu"]
+    }
+}, {
+    question_string: "Combien de mails sont envoyés chaque heure ?",
+    choices: {
+        correct: "10 milliards",
+        wrong: ["10 000", "10 millions", "100 millions"]
     }
 }];
 
 // An object for a Quiz, which will contain Question objects.
-var Quiz = function(quiz_name) {
+let Quiz = function(quiz_name) {
     // Private fields for an instance of a Quiz object.
     this.quiz_name = quiz_name;
 
@@ -45,14 +63,14 @@ var Quiz = function(quiz_name) {
 // A function that you can enact on an instance of a quiz object. This function is called add_question() and takes in a Question object which it will add to the questions field.
 Quiz.prototype.add_question = function(question) {
     // Randomly choose where to add question
-    var index_to_add_question = Math.floor(Math.random() * this.questions.length);
+    let index_to_add_question = Math.floor(Math.random() * this.questions.length);
     this.questions.splice(index_to_add_question, 0, question);
 }
 
-// A function that you can enact on an instance of a quiz object. This function is called render() and takes in a variable called the container, which is the <div> that I will render the quiz in.
+// A function that you can enact on an instance of a quiz object. This function is called render() and takes in a letiable called the container, which is the <div> that I will render the quiz in.
 Quiz.prototype.render = function(container) {
     // For when we're out of scope
-    var self = this;
+    let self = this;
 
     // Hide the quiz results modal
     $('#quiz-results').hide();
@@ -61,7 +79,7 @@ Quiz.prototype.render = function(container) {
     $('#quiz-name').text(this.quiz_name);
 
     // Create a container for questions
-    var question_container = $('<div class="jumbotron">').attr('id', 'question').insertAfter('#quiz-name');
+    let question_container = $('<div class="jumbotron shadow p-3 mb-5 bg-white rounded">').attr('id', 'question').insertAfter('#quiz-name');
 
     // Helper function for changing the question and updating the buttons
     function change_question() {
@@ -70,8 +88,8 @@ Quiz.prototype.render = function(container) {
         $('#next-question-button').prop('disabled', current_question_index === self.questions.length - 1);
 
         // Determine if all questions have been answered
-        var all_questions_answered = true;
-        for (var i = 0; i < self.questions.length; i++) {
+        let all_questions_answered = true;
+        for (let i = 0; i < self.questions.length; i++) {
             if (self.questions[i].user_choice_index === null) {
                 all_questions_answered = false;
                 break;
@@ -81,7 +99,7 @@ Quiz.prototype.render = function(container) {
     }
 
     // Render the first question
-    var current_question_index = 0;
+    let current_question_index = 0;
     change_question();
 
     // Add listener for the previous question button
@@ -103,41 +121,42 @@ Quiz.prototype.render = function(container) {
     // Add listener for the submit answers button
     $('#submit-button').click(function() {
         // Determine how many questions the user got right
-        var score = 0;
-        for (var i = 0; i < self.questions.length; i++) {
+        let score = 0;
+        for (let i = 0; i < self.questions.length; i++) {
             if (self.questions[i].user_choice_index === self.questions[i].correct_choice_index) {
                 score++;
             }
         }
 
         // Display the score with the appropriate message
-        var percentage = score / self.questions.length;
+        let percentage = score / self.questions.length;
         $.ajax({
             url: "result/" + percentage,
             success: function (data) {
             }
         });
         console.log(percentage);
-        var message;
+        let message;
         if (percentage === 1) {
-            message = 'Great job!'
+            message = 'WOW super!'
         } else if (percentage >= .75) {
-            message = 'You did alright.'
+            message = 'Beau score'
         } else if (percentage >= .5) {
-            message = 'Better luck next time.'
+            message = 'C\'est bien mais tu peux faire mieux.'
         } else {
-            message = 'Maybe you should try a little harder.'
+            message = 'Essaie peut-être au hasard.'
         }
         $('#quiz-results-message').text(message);
-        $('#quiz-results-score').html('You got <b>' + score + '/' + self.questions.length + '</b> questions correct.');
+        $('#quiz-results-score').html('Tu as obtenu <b>' + score + '/' + self.questions.length + '</b>' +
+            ' <questions></questions> correctes.');
         $('#quiz-results').slideDown();
         $('#quiz button').slideUp();
     });
 
     // Add a listener on the questions container to listen for user select changes. This is for determining whether we can submit answers or not.
     question_container.bind('user-select-change', function() {
-        var all_questions_answered = true;
-        for (var i = 0; i < self.questions.length; i++) {
+        let all_questions_answered = true;
+        for (let i = 0; i < self.questions.length; i++) {
             if (self.questions[i].user_choice_index === null) {
                 all_questions_answered = false;
                 break;
@@ -148,7 +167,7 @@ Quiz.prototype.render = function(container) {
 }
 
 // An object for a Question, which contains the question, the correct choice, and wrong choices. This block is the constructor.
-var Question = function(question_string, correct_choice, wrong_choices) {
+let Question = function(question_string, correct_choice, wrong_choices) {
     // Private fields for an instance of a Question object.
     this.question_string = question_string;
     this.choices = [];
@@ -158,13 +177,13 @@ var Question = function(question_string, correct_choice, wrong_choices) {
     this.correct_choice_index = Math.floor(Math.random() * wrong_choices.length + 1);
 
     // Fill in this.choices with the choices
-    var number_of_choices = wrong_choices.length + 1;
-    for (var i = 0; i < number_of_choices; i++) {
+    let number_of_choices = wrong_choices.length + 1;
+    for (let i = 0; i < number_of_choices; i++) {
         if (i === this.correct_choice_index) {
             this.choices[i] = correct_choice;
         } else {
             // Randomly pick a wrong choice to put in this index
-            var wrong_choice_index = Math.floor(Math.random(0, wrong_choices.length));
+            let wrong_choice_index = Math.floor(Math.random(0, wrong_choices.length));
             this.choices[i] = wrong_choices[wrong_choice_index];
 
             // Remove the wrong choice from the wrong choice array so that we don't pick it again
@@ -173,13 +192,13 @@ var Question = function(question_string, correct_choice, wrong_choices) {
     }
 }
 
-// A function that you can enact on an instance of a question object. This function is called render() and takes in a variable called the container, which is the <div> that I will render the question in. This question will "return" with the score when the question has been answered.
+// A function that you can enact on an instance of a question object. This function is called render() and takes in a letiable called the container, which is the <div> that I will render the question in. This question will "return" with the score when the question has been answered.
 Question.prototype.render = function(container) {
     // For when we're out of scope
-    var self = this;
+    let self = this;
 
     // Fill out the question label
-    var question_string_h2;
+    let question_string_h2;
     if (container.children('h2').length === 0) {
         question_string_h2 = $('<h2>').appendTo(container);
     } else {
@@ -190,14 +209,14 @@ Question.prototype.render = function(container) {
     // Clear any radio buttons and create new ones
     if (container.children('input[type=radio]').length > 0) {
         container.children('input[type=radio]').each(function() {
-            var radio_button_id = $(this).attr('id');
+            let radio_button_id = $(this).attr('id');
             $(this).remove();
             container.children('label[for=' + radio_button_id + ']').remove();
         });
     }
-    for (var i = 0; i < this.choices.length; i++) {
+    for (let i = 0; i < this.choices.length; i++) {
         // Create the radio button
-        var choice_radio_button = $('<input>')
+        let choice_radio_button = $('<input>')
             .attr('id', 'choices-' + i)
             .attr('type', 'radio')
             .attr('name', 'choices')
@@ -206,7 +225,7 @@ Question.prototype.render = function(container) {
             .appendTo(container);
 
         // Create the label
-        var choice_label = $('<label>')
+        let choice_label = $('<label>')
             .text(this.choices[i])
             .attr('for', 'choices-' + i)
             .appendTo(container);
@@ -214,7 +233,7 @@ Question.prototype.render = function(container) {
 
     // Add a listener for the radio button to change which one the user has clicked on
     $('input[name=choices]').change(function(index) {
-        var selected_radio_button_value = $('input[name=choices]:checked').val();
+        let selected_radio_button_value = $('input[name=choices]:checked').val();
 
         // Change the user choice index
         self.user_choice_index = parseInt(selected_radio_button_value.substr(selected_radio_button_value.length - 1, 1));
@@ -227,18 +246,18 @@ Question.prototype.render = function(container) {
 // "Main method" which will create all the objects and render the Quiz.
 $(document).ready(function() {
     // Create an instance of the Quiz object
-    var quiz = new Quiz('Test tes connaissances');
+    let quiz = new Quiz('QUIZZ');
 
     // Create Question objects from all_questions and add them to the Quiz object
-    for (var i = 0; i < all_questions.length; i++) {
+    for (let i = 0; i < all_questions.length; i++) {
         // Create a new Question object
-        var question = new Question(all_questions[i].question_string, all_questions[i].choices.correct, all_questions[i].choices.wrong);
+        let question = new Question(all_questions[i].question_string, all_questions[i].choices.correct, all_questions[i].choices.wrong);
 
         // Add the question to the instance of the Quiz object that we created previously
         quiz.add_question(question);
     }
 
     // Render the quiz
-    var quiz_container = $('#quiz');
+    let quiz_container = $('#quiz');
     quiz.render(quiz_container);
 });
