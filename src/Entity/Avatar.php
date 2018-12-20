add267954289de3db52df1af9c780e3e845774a4
Julie Delmas <julie.delmas33@gmail.com>
2018-12-20 15:34:25 +0100

    Making bdd.

--- src/Entity/Avatar.php
+++ src/Entity/Avatar.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -25,6 +27,16 @@ class Avatar
      * @ORM\Column(type="string", length=255)
      */
     private $fileName;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\User", mappedBy="avatar")
+     */
+    private $userAvatar;
+
+    public function __construct()
+    {
+        $this->userAvatar = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -51,6 +63,37 @@ class Avatar
     public function setFileName(string $fileName): self
     {
         $this->fileName = $fileName;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getUserAvatar(): Collection
+    {
+        return $this->userAvatar;
+    }
+
+    public function addUserAvatar(User $userAvatar): self
+    {
+        if (!$this->userAvatar->contains($userAvatar)) {
+            $this->userAvatar[] = $userAvatar;
+            $userAvatar->setAvatar($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserAvatar(User $userAvatar): self
+    {
+        if ($this->userAvatar->contains($userAvatar)) {
+            $this->userAvatar->removeElement($userAvatar);
+            // set the owning side to null (unless already changed)
+            if ($userAvatar->getAvatar() === $this) {
+                $userAvatar->setAvatar(null);
+            }
+        }
 
         return $this;
     }
